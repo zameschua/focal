f5cc652a0cc2c4bafebc691f9adb4babcf11e889
Zames <zames.chua@gmail.com>
2017-07-24 18:55:34 +0800

    Add fade effect on left and right panels in App.js

--- main/src/scripts/App.jsx
+++ main/src/scripts/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {TransitionMotion, spring} from 'react-motion';
+import Transition from 'react-transition-group/Transition';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'material-components-web/dist/material-components-web.css';
-import { autoInit } from 'material-components-web/dist/material-components-web.js';
-autoInit();
+import { autoInit } from 'material-components-web/dist/material-components-web';
 
 import TimeTrackerMain from './TimeTracker/components/TimeTrackerMain';
 import WelcomeCard from './WelcomeCard/containers/WelcomeCard';
@@ -19,6 +18,11 @@ class App extends Component {
     this.state = {
       sidePanelsVisible: false,
     };
+  }
+
+  componentDidMount() {
+    // Load up Material Components Web
+    autoInit();
   }
 
   showSidePanels() {
@@ -41,62 +45,109 @@ class App extends Component {
       }
 
       const wallpaperUrl = this.props.wallpaperUrl;
-      
+
       const backgroundImageStyle = {
         background: `url(${wallpaperUrl})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center",
-        backgroundAttachment: "fixed",
-        backgroundSize: "cover",
-        height: "100vh",
-        overflow: "hidden",
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        backgroundAttachment: 'fixed',
+        backgroundSize: 'cover',
+        height: '100vh',
+        overflow: 'hidden',
       };
 
-      let leftPanel = null;
-      let rightPanel = null;
+      const transitionDuration = 200;
 
-      if (this.state.sidePanelsVisible) {
-        leftPanel = (
-          <div className="container" style={{paddingTop: "5%"}}>
-            <EventsFeed />
-          </div>
-        );
-        rightPanel = (
-          <div className="container" style={{paddingTop: "5%"}}>
-            <div className="row">
-              <TimeTrackerMain />
+      const leftPanelDefaultStyles = {
+        transition: `opacity ${transitionDuration}ms ease-in-out`,
+        opacity: 0,
+        marginTop: '5%',
+      };
+
+      const rightPanelDefaultStyles = {
+        transition: `opacity ${transitionDuration}ms ease-in-out`,
+        opacity: 0,
+        marginTop: '5%',
+      };
+
+      const leftPanelTransitionStyles = {
+        entering: { opacity: 1 },
+        entered: { opacity: 1 },
+        exiting: { opacity: 0 },
+        exited: { opacity: 0 },
+      };
+
+      const rightPanelTransitionStyles = {
+        entering: { opacity: 1 },
+        entered: { opacity: 1 },
+        exiting: { opacity: 0 },
+        exited: { opacity: 0 },
+      };
+
+      const leftPanel = (
+        <Transition in={this.state.sidePanelsVisible} timeout={transitionDuration} >
+          {state => (
+            <div
+              className="container"
+              style={{
+                ...leftPanelDefaultStyles,
+                ...leftPanelTransitionStyles[state],
+              }}
+            >
+              <EventsFeed />
             </div>
-            <br />
-            <div className="row">
-              <HabitsTrackerApp/>
+          )}
+        </Transition>
+      );
+
+      const rightPanel = (
+        <Transition in={this.state.sidePanelsVisible} timeout={transitionDuration} >
+          {state => (
+            <div
+              className="container"
+              style={{
+                ...rightPanelDefaultStyles,
+                ...rightPanelTransitionStyles[state],
+              }}
+            >
+              <div className="row">
+                <TimeTrackerMain />
+              </div>
+              <br />
+              <div className="row">
+                <HabitsTrackerApp/>
+              </div>
             </div>
-          </div>
-        );
-      }
+          )}
+        </Transition>
+      );
 
       return (
-        <div className="App container-fluid" style={{padding: "0"}}> {/* Attatch background image */}
+        <div className="App container-fluid" style={{ padding: '0' }}>
           <div style={backgroundImageStyle}>
             <div className="row h-100">
-              
-              <div className="col-3" 
-                style={{height: "100vh", paddingRight: "0"}}
+
+              <div 
+                className="col-3"
+                style={{ height: '100vh', paddingRight: '0' }}
                 onMouseEnter={this.showSidePanels.bind(this)}
-                onMouseLeave={this.hideSidePanels.bind(this)}>
+                onMouseLeave={this.hideSidePanels.bind(this)}
+              >
                 {leftPanel}
               </div>
 
               <div className="col-6 align-self-center">
                 <div className="text-center">
-                  <WelcomeCard userName={this.props.userName}/>
+                  <WelcomeCard userName={ this.props.userName }/>
                 </div>
                 
               </div>
 
               <div className="col-3"
-                style={{height:"100vh", paddingLeft: "0"}}
+                style={{ height: '100vh', paddingLeft: '0' }}
                 onMouseEnter={this.showSidePanels.bind(this)}
-                onMouseLeave={this.hideSidePanels.bind(this)}>
+                onMouseLeave={this.hideSidePanels.bind(this)}
+              >
                 {rightPanel}
               </div>
 
@@ -109,7 +160,7 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   // Check if the state is ready
   if (state.appState) {
     return {
@@ -117,23 +168,19 @@ const mapStateToProps = state => {
       userFirstLogin: (state.appState.userName === ''), // Returns true if user first login
       userName: state.appState.userName,
       wallpaperUrl: state.appState.cachedWallpapers.nextWallpaper,
-      storeHasLoaded: !(state.appState === undefined),
-    };
-  } else {
-    return {
-      sidePanelsVisible: false,
-      userFirstLogin: true,
-      userName: "",
-      wallpaperUrl: "",
+      storeHasLoaded: true,
     };
   }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {};
+  return {
+    sidePanelsVisible: false,
+    userFirstLogin: true,
+    userName: '',
+    wallpaperUrl: '',
+    storeHasLoaded: false,
+  };
 };
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  null,
 )(App);
