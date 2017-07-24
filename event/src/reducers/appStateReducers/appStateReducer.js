1c535d10437525efaa7f83f9cb359e903ad416d0
Zames <zames.chua@gmail.com>
2017-07-24 15:18:13 +0800

    Remove showSidePanels and hideSidePanels logic from the state

--- event/src/reducers/appStateReducers/appStateReducer.js
+++ event/src/reducers/appStateReducers/appStateReducer.js
@@ -1,16 +1,17 @@
-import sidePanelsReducer from './sidePanelsReducer';
 import userNameReducer from './userNameReducer';
 import wallpapersReducer from './wallpapersReducer';
 
 const INITIAL_STATE = {
-	sidePanelsVisible: false,
 	userName: "",
+		cachedWallpapers: {
+			wallpaperQueue: [],
+			nextWallpaper: "",
+		},
 }
 
 // Combine reducers for todoList
 function appStateReducer(state = INITIAL_STATE, action) {
   return {
-  	sidePanelsVisible: sidePanelsReducer(state.sidePanelsVisible, action),
     userName: userNameReducer(state.userName, action), // ADD THIS LATER
   	cachedWallpapers: wallpapersReducer(state.cachedWallpapers, action),
   }
