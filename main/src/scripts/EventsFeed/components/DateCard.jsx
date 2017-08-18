9f7b7fc22c7e8592dc53347e988c0c1c8a17f7ec
Zames <zames.chua@gmail.com>
2017-08-18 14:39:55 +0800

    Minor css changes

--- main/src/scripts/EventsFeed/components/DateCard.jsx
+++ main/src/scripts/EventsFeed/components/DateCard.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const DateCard = ({ date, month, day }) => (
   <div className="container" style={{ paddingTop: '3vh' }}>
     <div
-      className="row mdc-typography--title mdc-theme--text-primary-on-dark"
-      style={{ textShadow: '1px 1px 0.5px #777' }}
+      className="row mdc-typography--subheading2 mdc-theme--text-primary-on-dark"
+      style={{ textShadow: '1px 1px 0.5px #777'}}
     >
       {`${date} ${month}`}
     </div>
